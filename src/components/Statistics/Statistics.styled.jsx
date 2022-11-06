06f40d937f8d6c662c4bff3d29df162caf77c32c
Yurii-Naumchuk <98982620+Yurii-Naumchuk@users.noreply.github.com>
2022-11-06 15:25:26 +0200

    fd

--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -3,16 +3,16 @@ import styled from 'styled-components';
 export const StatisticsThumb = styled.div`
   justify-content: center;
   display: flex;
-`;
+  `;
 export const StatisticsItem = styled.ul`
   display: flex;
 `;
 export const StatisticsList = styled.li`
   margin-right: 15px;
-  border: 2px solid #bfe2ff;
-  background: #33b766;
+  border: none;
+  background: #33b788;
   font-family: 'Roboto Condensed', Тahoma, sans-serif;
   color: #fff;
-  padding: 12px 30px;
-  border-radius: 10px;
+  padding: 10px 20px;
+  border-radius: 5px;
 `;
